91fb38c43215c8a040ed13c384a80caabc217ae1
KyawHtet8 <nto4777@gmail.com>
2025-03-19 15:13:50 +0700

    initial commit

---
+++ verify_installation.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+from PIL import Image
+import matplotlib.pyplot as plt
+
+print("TensorFlow version:", tf.__version__)
+print("NumPy version:", np.__version__)
+print("OpenCV version:", cv2.__version__)
+print("Pillow version:", Image.__version__)
+#print("Matplotlib version:", plt.__version__)
